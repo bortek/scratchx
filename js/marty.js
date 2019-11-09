a888aaaffec71c18da58c6aed11588a8c52cc785
Bortek <bortek666@gmail.com>
2019-11-09 09:15:01 +0100

    Update marty.js

--- js/marty.js
+++ js/marty.js
@@ -343,7 +343,7 @@ function Marty(IP, name){
   }
 
   this.motor_protection = function(enabled){
-    if (enabled === undefined || (enabled != true && enabled != false)){enabled = true;} else {enabled = false;}
+    if (enabled === undefined || (enabled != true && enabled != false)){enabled = true;} else {enabled = False;}
     this.socket.send(new Uint8Array([0x02, 0x02, 0x00, 0x16, enabled]));
   }
 
